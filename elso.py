ed9833cb640693ac3ad58680a61610c3b950ef7e
Heysko <ronyaizsolt@hotmail.com>
2023-09-14 16:23:43 +0200

    done for now

--- elso.py
+++ elso.py
@@ -1,1 +1,15 @@
-print('Üdvözöllek')
+print('Üdvöz\nöllek')
+print('Szia','Jenő', sep='-------')
+print('Ez'
+      'több'
+      'sorba'
+      'kerül')
+nev='Julia'
+titulus='dr.'
+print('Szia', nev)
+print(f'Szia {nev}')
+print('hello {0}'.format(nev))
+print('hello {0} {1}'.format(titulus,nev))
+
+print('jobbra'.rjust(50,'-'))
+print('balra'.ljust(50,'-'))
